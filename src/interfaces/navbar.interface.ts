d555820c4914f03a72c908fb0bc124a423537504
khh-Niloy <niloy.dev.101@gmail.com>
2025-07-03 21:49:53 +0600

    setup: page routing setup

---
+++ src/interfaces/navbar.interface.ts
@@ -0,0 +1,4 @@
+export interface INavbar {
+  label: string;
+  links: string;
+}
--- src/routes/route.ts
+++ src/routes/route.ts
@@ -1,10 +1,31 @@
 import { createBrowserRouter } from "react-router";
 // import App from "../App";
 import MainLayout from "../layout/MainLayout";
+import Books from "@/Pages/Books";
+import CreateBook from "@/Pages/CreateBook";
+import BorrowSummary from "@/Pages/BorrowSummary";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     Component: MainLayout,
+    children: [
+      {
+        path: "all-books",
+        Component: Books,
+      },
+      {
+        path: "create-book",
+        Component: CreateBook,
+      },
+      {
+        path: "borrow-summary",
+        Component: BorrowSummary,
+      },
+      // {
+      //   path: "/create-book",
+      //   Component: CreateBook
+      // },
+    ],
   },
 ]);
